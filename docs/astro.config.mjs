3d5a8b96e241f14046880d3d67a53b1fc5faba55
Sayafdine Said <sayafdine.said@outlook.fr>
2024-10-07 00:45:39 +0200

    docs: fix link (#12)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://saidsay-so.github.io/gateau",
+  site: "https://saidsay-so.github.io",
   integrations: [
     starlight({
       title: "gateau",
