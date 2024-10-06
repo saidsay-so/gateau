8ce9af13cbb9c618f72bae18deefbb7b95b3b28d
Sayafdine Said <sayafdine.said@outlook.fr>
2024-10-07 00:49:23 +0200

    docs: add missing prop (#13)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://saidsay-so.github.io",
+  base: "/gateau/",
   integrations: [
     starlight({
       title: "gateau",
