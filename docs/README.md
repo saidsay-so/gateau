62d49d39467be5ddc3d14cb77266d6eb208bb7fa
Sayafdine Said <sayafdine.said@outlook.fr>
2024-10-07 00:37:49 +0200

    docs: add documentation website (#11)

---
+++ docs/README.md
@@ -0,0 +1,12 @@
+# gateau documentation
+
+This is the documentation website for gateau.
+It is built using [Astro](https://astro.build/),
+a modern static site generator for building websites,
+with the help of [Starlight](https://starlight.astro.build/),
+a documentation theme for Astro.
+
+## Contributing
+
+If you would like to contribute to the documentation,
+please refer to the [CONTRIBUTING.md](/CONTRIBUTING.md) file.
---
+++ docs/astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://saidsay-so.github.io/gateau",
+  integrations: [
+    starlight({
+      title: "gateau",
+      logo: {
+        src: "./src/assets/logo.svg",
+      },
+      social: {
+        github: "https://github.com/saidsay-so/gateau",
+      },
+      editLink: {
+        baseUrl: "https://github.com/saidsay-so/gateau/edit/main/docs",
+      },
+      sidebar: [
+        {
+          label: "First Steps",
+          autogenerate: { directory: "first-steps" },
+        },
+      ],
+    }),
+  ],
+});
